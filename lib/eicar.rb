d15b30c1855ab11ee54debba49de9977373a977c
Nathan Clark <Nathan.Clark@nasa.gov>
2016-04-01 13:22:04 -0400

    Fixing test string.

--- lib/eicar.rb
+++ lib/eicar.rb
@@ -1,15 +1,15 @@
 require "tempfile"
 
 module Eicar
-  TEST_STRING = "X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"
+  TEST_STRING = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"
 
-  # Instantiates an "infected" file in a temp folder, passes it to the block,
-  # and then deletes it afterwards.
+  # Instantiates an "infected" file in a temp folder, passes its path, and then
+  # deletes it afterwards.
   def self.with_fake_virus(&block)
     Tempfile.open("eicar_test_virus") do |f|
       f.write(TEST_STRING)
-      f.rewind
-      block.call(f)
+      f.close
+      block.call(f.path)
     end
   end
 end
